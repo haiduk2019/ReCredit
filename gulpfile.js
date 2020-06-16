c5dccbfe2bf6e170d600f2d89326a74a90d7abf6
Iryna <irinahaiduk@ukr.net>
2020-06-16 11:05:25 +0300

    Add promo and footer on main page

--- gulpfile.js
+++ gulpfile.js
@@ -96,11 +96,11 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/img/**/*.{jpg,png,jpeg}", gulp.series("html", "refresh"));
+  gulp.watch("source/img/*.{jpg,png,jpeg}", gulp.series("html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/*.html", gulp.series("minify-html"));
-  gulp.watch("source/*.js", gulp.series("html", "refresh"));
-  gulp.watch("source/*.js", gulp.series("compress"));
+  gulp.watch("source/js/*.js", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("compress"));
 
 });
 
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,12 +1,46 @@
-let navMain = document.querySelector('.js-main-nav');
-let navToggle = document.querySelector('.js-main-nav__toggle');
-let langMenu = document.querySelector('.js-lang-menu');
+let navMain = $('.js-main-nav');
+let navToggle = $('.js-main-nav__toggle');
+let langMenu = $('.js-lang-menu');
 
 
-navToggle.addEventListener('click', () => {
-  navToggle.classList.toggle('main-nav__toggle--active');
-  navMain.classList.toggle('main-nav--active');
-  langMenu.classList.toggle('lang-menu--active');
+navToggle.on('click', function() {
+  navToggle.toggleClass('main-nav__toggle--active');
+  navMain.toggleClass('main-nav--active');
+  langMenu.toggleClass('.lang-menu--active');
 });
 
 
+
+
+$( function() {
+
+
+
+$( "#slider-range-min" ).slider({
+    range: "min",
+    value: 10000,
+    min: 10000,
+    max: 100000,
+    slide: function( event, ui ) {
+      $( "#test1" ).text( "$" + ui.value );
+    }
+  });
+
+
+  $( "#slider-range-min22" ).slider({
+    range: "min",
+    value: 3,
+    step: 3,
+    min: 3,
+    max: 12,
+    slide: function( event, ui ) {
+      $( "#test111" ).text( "$" + ui.value );
+    }
+  });
+
+  $( "#test1" ).text( "$" + $( "#slider-range-min" ).slider( "value" ) );
+  $( "#test111" ).text( "$" + $( "#slider-range-min22" ).slider( "value" ) );
+
+
+
+} );

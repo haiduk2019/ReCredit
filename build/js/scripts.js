2dd81bbb13fa3e4e46a7b069879bbc7f01aac61c
Iryna <irinahaiduk@ukr.net>
2020-06-27 17:48:26 +0300

    Add credit-products page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -13,6 +13,26 @@ checkMark.on('click', function () {
   $(this).siblings(answer).toggleClass('reference-container__answer--active');
 });
 
+var conditionsBtn = $('.js-display-toggle');
+
+conditionsBtn.on('click', function () {
+  var hiddenBtn =  $(this).siblings('.js-display-toggle:hidden');
+  $(this).css('display', 'none');
+  hiddenBtn.css('display', 'flex');
+  // $(this).siblings('.js-conditions').toggleClass('credit-product__wrapper--hidden');
+  var conditions = $(this).siblings('.js-conditions');
+  if(conditions.is(":visible")) {
+    conditions.hide(1000);
+  } else {
+    conditions.show(1000);
+  }
+  //  conditions.css('display', 'block');
+//   $(this).css('display', 'none');
+//  hideConditions.css('display', 'flex');
+});
+
+
+
 var langMenu = $('.js-lang-menu');
 var navHeader = $('.js-main-nav');
 var navToggle = $('.js-main-nav__toggle');
--- source/js/check-mark.js
+++ source/js/check-mark.js
@@ -5,3 +5,23 @@ checkMark.on('click', function () {
   $(this).toggleClass('reference-container__check-mark--active');
   $(this).siblings(answer).toggleClass('reference-container__answer--active');
 });
+
+var conditionsBtn = $('.js-display-toggle');
+
+conditionsBtn.on('click', function () {
+  var hiddenBtn =  $(this).siblings('.js-display-toggle:hidden');
+  $(this).css('display', 'none');
+  hiddenBtn.css('display', 'flex');
+  // $(this).siblings('.js-conditions').toggleClass('credit-product__wrapper--hidden');
+  var conditions = $(this).siblings('.js-conditions');
+  if(conditions.is(":visible")) {
+    conditions.hide(1000);
+  } else {
+    conditions.show(1000);
+  }
+  //  conditions.css('display', 'block');
+//   $(this).css('display', 'none');
+//  hideConditions.css('display', 'flex');
+});
+
+

e0dfc962b17dbb75953ca8415945b1c518d33960
Iryna <irinahaiduk@ukr.net>
2020-07-01 13:40:56 +0300

    Edit pay-loan page

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -11,6 +11,23 @@ navToggle.on('click', function () {
   langMenu.toggleClass('lang-menu--active');
 });
 
+var payToggle = $('.js-payment-method__toggle');
+var payArrow =$('.js-payment-method__arrow');
+var payInfo = $('.js-instruction-info');
+
+payToggle.on('click', function () {
+  payToggle.removeClass('payment-method__toggle--active');
+  $(this).addClass('payment-method__toggle--active');
+
+/*   payArrow.toggleClass('payment-method__arrow--active'); */
+if (payInfo.hasClass('instruction-info--active')) {
+  // payInfo.removeClass('instruction-info--active');
+} else {
+  payInfo.addClass('instruction-info--active');
+}
+/*   payInfo.toggleClass('instruction-info--active'); */
+});
+
 //Маска ввода номера на сайте
 // $('input[name=phone-field]').mask("+3 (999) 999-99-99");
 

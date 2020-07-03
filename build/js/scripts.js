2b36c2f9a05a1242d563a3440991eb2099b60736
Iryna <irinahaiduk@ukr.net>
2020-07-03 21:19:46 +0300

    Add how-to-get-loan page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -109,8 +109,26 @@ $( document ).ready(function() {
 
   select.on( "change", function() {
     $('.js-credit-calculate__period').text(this.value + ' мес');
-  //  $('.js-credit-calculate__period').text()
-    slider.slider( "value", this.selectedIndex + 1 );
+    slider.slider( "value", this.value);
   });
 
 });
+////
+
+
+
+$( document ).ready(function() {
+  $( ".credit-calculate__range--getting-info" ).slider({
+      range: "min",
+      value: 10000,
+      min: 10000,
+      max: 100000,
+      slide: function( event, ui ) {
+        $( ".credit-calculate__amount" ).text(ui.value + " грн");
+      }
+    });
+    $( ".credit-calculate__amount" ).text($( ".credit-calculate__range" ).slider( "value" )+ " грн" );
+  });
+
+
+

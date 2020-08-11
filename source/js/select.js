b462250acf4e5f07eb100dbf6a5147bc8123b846
oz <lisiy50@gmail.com>
2020-08-11 17:25:37 +0300

    fix

--- source/js/select.js
+++ source/js/select.js
@@ -9,5 +9,20 @@ designedSelect.selectmenu({
     "ui-selectmenu-menu": "term__wrapper"
   },
   change: function( event, ui ) {
+    if ($(this).attr('id') == 'request_for_a_loan_employment') {
+      if ($(this).val() == 'Прочее' || $(this).val() == 'Інше') {
+        $('#employmentManual-wrapper').show();
+      } else {
+        $('#employmentManual-wrapper').hide();
+      }
+    }
+    if ($(this).attr('id') == 'request_for_a_loan_purposeOfTheLoan') {
+      if ($(this).val() == 'Прочее' || $(this).val() == 'Інше') {
+        $('#purposeOfTheLoanManual-wrapper').show();
+      } else {
+        $('#purposeOfTheLoanManual-wrapper').hide();
+      }
+    }
   }
 });
+designedSelect.selectmenu().change();
